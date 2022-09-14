098ac317b30b33f3db2dcdd49ed8c6af33894624
tentationcosmetic <109521605+tentationcosmetic@users.noreply.github.com>
2022-09-14 14:49:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,38 +12,32 @@ $(document).ready(function() {
                 }
             }
         }
+    }
+
+    // Ajout d'un bouton pour vider le panier
+    if ($('#cart_summary').length >= 1) {
+        $('#cart_summary').append('<button class="cart_empty btn btn-primary ">Vider le panier</button>');
+    }
 
 
-        // Ajout d'un bouton pour vider le panier
-        if ($('#cart_summary').length >= 1) {
-            $('#cart_summary').append('<button class="cart_empty btn btn-primary ">Vider le panier</button>');
+    $('.cart_empty').on('click', function() {
+        if (window.confirm("Souhaitez-vous vider votre panier ?")) {
+            $('.cart_quantity_delete').each(function() {
+                $(this).click();
+            });
+        }
+    });
+
+    $('#contact #id_contact').on('change', function() {
+        if ($('#id_contact').val() == '3') {
+            $("#fileUpload").prop("required", true);
+        } else {
+            $("#fileUpload").prop("required", false)
         }
 
+    });
 
-        $('.cart_empty').on('click', function() {
-            if (window.confirm("Souhaitez-vous vider votre panier ?")) {
-                $('.cart_quantity_delete').each(function() {
-                    $(this).click();
-                });
-            }
-        });
+    $('tr[id$="gift"] a').attr('href', '#');
 
-        $('#contact #id_contact').on('change', function() {
-            if ($('#id_contact').val() == '3') {
-                $("#fileUpload").prop("required", true);
-            } else {
-                $("#fileUpload").prop("required", false)
-            }
-
-        });
-
-        $('tr[id$="gift"] a').attr('href', '#');
-
-        $("<div class='page-subheading' data-axseo-oldtag='h3'>Vous avez déjà commandé chez nous ? <br> <br>Votre compte existe déjà ! <br> <br><a class='cauth-btn btn btn-lg btn-block btn-blue' href='https://drive.google.com/file/d/1OfAPkjT0Lft38dbDRYpbQp_Ii9T39qp1/view?usp=sharing' target='_blank'>Découvrez comment vous connecter !</a></p>").insertBefore("#authentication .page-subheading:first-child");
-
-
-
-
-
-    }
+    $("<div class='page-subheading' data-axseo-oldtag='h3'>Vous avez déjà commandé chez nous ? <br> <br>Votre compte existe déjà ! <br> <br><a class='cauth-btn btn btn-lg btn-block btn-blue' href='https://drive.google.com/file/d/1OfAPkjT0Lft38dbDRYpbQp_Ii9T39qp1/view?usp=sharing' target='_blank'>Découvrez comment vous connecter !</a></p>").insertBefore("#authentication .page-subheading:first-child");
 });
